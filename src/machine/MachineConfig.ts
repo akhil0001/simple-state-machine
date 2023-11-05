8656c780bc7dc7f669800597d8fa9142de517d5e
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-05 21:31:33 +0530

    feat: add drag and drop example

--- src/machine/MachineConfig.ts
+++ src/machine/MachineConfig.ts
@@ -1,7 +1,7 @@
 import { State } from "./State";
 import { IDefaultEvent, TDefaultContext, TDefaultStates } from "./types";
 
-type TStates<T extends readonly string[], IContext, IEvents> = {
+type TStates<T extends readonly string[], IContext, IEvents extends IDefaultEvent> = {
     [TIndex in T[number]]: State<IContext, T, IEvents>
 } | Record<string, State<IContext, T, IEvents>>
 
