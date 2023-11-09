1169bce3b67d06bbc95b66e2976fc01914753439
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-09 09:25:42 +0530

    feat: introdue rectangle and tooltip components

---
+++ src/components/ToolTip.tsx
@@ -0,0 +1,7 @@
+export const ToolTip = () => {
+    return (
+        <div id="positionTooltip"
+            style={{ position: "absolute", left: '0px', top: '250px', backgroundColor: 'black', color: 'white', padding: '20px', borderRadius: '20px' }}>
+        </div>
+    )
+}
