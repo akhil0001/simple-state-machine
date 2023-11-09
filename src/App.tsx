7b849159c64e7ccb6dbbceda2da635de41ac5655
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-09 09:26:04 +0530

    update: use rectangle and tooltip in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import 'reactflow/dist/style.css';
-import { Inspect } from './machine/inspect'
-import { inspect, subscribe } from './drag-and-drop.ts'
+// import { Inspect } from './machine/inspect'
+// import { inspect, subscribe } from './drag-and-drop.ts'
+import { Rectangle } from './components/Rectangle.tsx';
+import { ToolTip } from './components/ToolTip.tsx';
 // import { inspect, subscribe } from '.'
 
 function App() {
 
   return (
     <>
-      <Inspect inspect={inspect} subscribe={subscribe} />
+      <Rectangle />
+      <ToolTip />
+      {/* <Inspect inspect={inspect} subscribe={subscribe} /> */}
     </>
   )
 }
