235353a86504be379024e5a46659d964a83efba1
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-10 08:30:01 +0530

    feat: add support for passing context

--- src/machine/createMachine.ts
+++ src/machine/createMachine.ts
@@ -52,9 +52,9 @@ type TInternalState = 'entered' | 'living' | 'exited' | 'dead'
 type TSubscriberType = 'allChanges' | 'stateChange' | 'contextChange'
 
 
-export function createMachine<U extends TDefaultContext, V extends TDefaultStates, W extends IDefaultEvent>(config: MachineConfig<U, V, W>): TCreateMachineReturn<U, V, W> {
+export function createMachine<U extends TDefaultContext, V extends TDefaultStates, W extends IDefaultEvent>(config: MachineConfig<U, V, W>, context: U = {} as U): TCreateMachineReturn<U, V, W> {
     const { states, context: initialContext } = config;
-    let _context = initialContext;
+    let _context = { ...initialContext, ...context };
     const initialStateValue: keyof typeof states = Object.keys(states)[0];
     let _currentState = states[initialStateValue]
     const currentState = {
