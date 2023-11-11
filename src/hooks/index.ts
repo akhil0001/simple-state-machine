940d8188876838ef428c1b469bc5311548eb8d90
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-11 13:55:11 +0530

    update: add eslint-disable for exhaustive deps

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,6 +6,7 @@ import { IDefaultEvent, TDefaultContext, TDefaultStates } from "../machine/types
 export function useMachine<U extends TDefaultContext, V extends TDefaultStates, W extends IDefaultEvent>(machineConfig: MachineConfig<U, V, W>, context: Partial<U> = {} as U) {
     const { state: initialState, send, subscribe, start } = useMemo(() => {
         return createMachine(machineConfig, context);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [machineConfig])
     const [state, setState] = useState(initialState);
 
