2b20dfd932e90a6f71627957bc88a3dcce432624
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-12 14:34:20 +0530

    update: App with containers

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import './App.css'
 import 'reactflow/dist/style.css';
 // import { Inspect } from './machine/inspect'
 // import { inspect, subscribe } from './drag-and-drop.ts'
-import { Rectangle } from './components/Rectangle.tsx';
-import { ToolTip } from './components/ToolTip.tsx';
+// import { Rectangle } from './components/Rectangle.tsx';
+// import { ToolTip } from './components/ToolTip.tsx';
 import { TimerContainer } from './components/TimerContainer.tsx';
+import { FetchContainer } from './components/FetchContainer.tsx';
+import { DebounceContainer } from './components/DebounceContainer.tsx';
 // import { inspect, subscribe } from '.'
 
 function App() {
@@ -14,6 +16,10 @@ function App() {
       {/* <Rectangle />
       <ToolTip /> */}
       <TimerContainer initialTime={10} />
+      <hr />
+      <FetchContainer />
+      <hr />
+      <DebounceContainer />
       {/* <Inspect inspect={inspect} subscribe={subscribe} /> */}
     </>
   )
