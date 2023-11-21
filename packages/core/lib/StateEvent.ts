d0a20b89c2da72cc3fffc47693b2d5d0b94de81b
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-21 20:34:18 +0530

    refactor: introduce updateStateJSON

--- packages/core/lib/StateEvent.ts
+++ packages/core/lib/StateEvent.ts
@@ -4,17 +4,21 @@ import { IDefaultEvent, TStateEvent, TStateEventCallback } from "./types";
 type TStateEventCollection<IContext, IEvents extends IDefaultEvent> = TStateEvent<IContext, IEvents>[]
 export class StateEvent<IContext, IEvents extends IDefaultEvent> {
     stateEventCollection: TStateEventCollection<IContext, IEvents> = []
-    constructor() {
+    #updateStateJSON: () => void;
+    constructor(updateStateJSON: () => void) {
         this.stateEventCollection = [];
+        this.#updateStateJSON = updateStateJSON;
     }
     fireAndForget(cb: TStateEventCallback<IContext, IEvents, void>) {
         const oldStateEventCollection = this.stateEventCollection;
         this.stateEventCollection = [...oldStateEventCollection, { type: 'fireAndForget', callback: cb }]
+        this.#updateStateJSON()
         return this
     }
     updateContext(cb: TStateEventCallback<IContext, IEvents, IContext>) {
         const oldStateContextCollection = this.stateEventCollection;
         this.stateEventCollection = [...oldStateContextCollection, { type: 'updateContext', callback: cb }]
+        this.#updateStateJSON()
         return this
     }
 }
