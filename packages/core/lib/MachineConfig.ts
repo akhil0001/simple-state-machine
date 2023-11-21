7f5ca0b6b2825d38e84055e10fabcb63ce8a9b0f
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-21 20:39:25 +0530

    refactor: remove `on` method from machineConfig

--- packages/core/lib/MachineConfig.ts
+++ packages/core/lib/MachineConfig.ts
@@ -1,27 +1,9 @@
-import { Action, TIf, TMoveTo, TStateJSONPayload } from "./Action";
 import { State } from "./State";
-import { StateEvent } from "./StateEvent";
-import { IDefaultEvent, TDefaultContext, TDefaultStates, TStateEventCallback } from "./types";
+import { IDefaultEvent, TDefaultContext, TDefaultStates } from "./types";
 
 type TStates<T extends readonly string[], IContext, IEvents extends IDefaultEvent> = {
     [TIndex in T[number]]: State<T, IContext, IEvents>
 }
-
-type TConvertArrToObj<TArr extends readonly string[]> = {
-    [TIndex in TArr[number]]: TArr[number]
-}
-
-type TCond<IContext> = (context: IContext) => boolean;
-
-type TTargetState<AllStates extends readonly string[]> = keyof TConvertArrToObj<AllStates>;
-
-type TActionType = string | symbol;
-
-type TStatesJSON<IContext, IStates extends TDefaultStates, IEvents extends IDefaultEvent> = {
-    [key in IEvents[number] | symbol]: Array<TStateJSONPayload<IContext, IStates>>;
-};
-
-const returnTrue = () => true;
 
 export function createEvents<T extends readonly string[]>(...args: T) {
     return args
@@ -29,8 +11,6 @@ export function createEvents<T extends readonly string[]>(...args: T) {
 export class MachineConfig<IStates extends TDefaultStates, IContext extends TDefaultContext, IEvents extends IDefaultEvent> {
     #states: TStates<IStates, IContext, IEvents> = {} as TStates<IStates, IContext, IEvents>;
     #context: IContext;
-    #stateJSON: TStatesJSON<IContext, IStates, IEvents> = {} as TStatesJSON<IContext, IStates, IEvents>;
-    #stateEventsMap: Map<TActionType, StateEvent<IContext, IEvents>> = new Map();
     #actions: IEvents;
 
     constructor(states: IStates, newContext: IContext, actions: IEvents) {
@@ -52,51 +32,13 @@ export class MachineConfig<IStates extends TDefaultStates, IContext extends TDef
         return this.#states
     }
 
-    #updateStateJSON(actionType: IEvents[number] | symbol, payload: {
-        target: TTargetState<IStates>,
-        cond: TCond<IContext>,
-        isSetByDefault: boolean
-    }) {
-        const prev = this.#stateJSON[actionType];
-        this.#stateJSON[actionType] = [...prev, payload]
-    }
-
-
-    on(actionType: IEvents[number]): {
-        moveTo: TMoveTo<IContext, IStates, IEvents>,
-        if: TIf<IContext, IStates, IEvents>,
-        fireAndForget: (cb: TStateEventCallback<IContext, IEvents, void>) => StateEvent<IContext, IEvents>,
-        updateContext: (cb: TStateEventCallback<IContext, IEvents, IContext>) => StateEvent<IContext, IEvents>
-    } {
-        const stateEvent = new StateEvent<IContext, IEvents>()
-        const newStateJSONPayload: TStateJSONPayload<IContext, IStates> = {
-            cond: returnTrue,
-            isSetByDefault: true,
-            target: '##notYetDeclared##'
-        }
-        if (Object.keys(this.#stateJSON).includes(actionType)) {
-            this.#stateJSON[actionType].push(newStateJSONPayload)
-        }
-        else {
-            this.#stateJSON[actionType] = [newStateJSONPayload]
-        }
-        const boundUpdateStateJSON = this.#updateStateJSON.bind(this);
-        const action = new Action<IContext, IStates, IEvents>(actionType, boundUpdateStateJSON, stateEvent, newStateJSONPayload)
-        this.#stateEventsMap.set(actionType, stateEvent);
-        const returnActions = action.returnStateEventActions()
-        const boundIf = action.if.bind(action);
-        const boundMoveTo = action.moveTo.bind(action)
-        return { moveTo: boundMoveTo, if: boundIf, ...returnActions }
-    }
 
     getConfig() {
         const actions = this.#actions;
         const states = this.#states;
         const context = this.#context
-        const stateEventsMap = this.#stateEventsMap;
-        const stateJSON = this.#stateJSON
         return {
-            states, context, stateEventsMap, stateJSON, actions
+            states, context, actions
         }
     }
 
