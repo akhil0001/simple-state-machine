bcadf4cdd6f9a41a98fab4e183c71a8ef28c6f29
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-23 10:55:22 +0530

    feat: export assign

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,4 +1,5 @@
 export { createMachine } from "./createMachine";
 export { MachineConfig } from "./MachineConfig";
 export { createStates, createEvents } from './utils'
+export { assign } from './assign'
 export * from './types'
