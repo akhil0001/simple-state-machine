bcdf4050fdaddd23a7495eff24c019be7bc9f286
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-23 11:28:25 +0530

    update: call using assign function

--- packages/react/src/machines/debounceMachine.ts
+++ packages/react/src/machines/debounceMachine.ts
@@ -1,4 +1,4 @@
-import { MachineConfig, createEvents, createStates } from "@simple-state-machine/core";
+import { MachineConfig, assign, createEvents, createStates } from "@simple-state-machine/core";
 
 function eitherOr<T, U>(arg1: T, arg2: U): T | U {
     return arg1 || arg2
@@ -8,18 +8,24 @@ const states = createStates('idle', 'debouncing', 'fetching', 'error')
 
 const events = createEvents('updateInput')
 
-export const debounceMachine = new MachineConfig(states, { response: eitherOr({ id: 0, description: 0 }, {}), input: 0 }, events)
+const context = { response: eitherOr({ id: 0, description: 0 }, {}), input: 0 }
+
+export const debounceMachine = new MachineConfig(states, context, events)
 
 const { idle, debouncing, fetching } = debounceMachine.getStates()
 
-idle.on('updateInput').updateContext((context, event) => ({ ...context, input: event.data.input }))
+const updateInput = assign<typeof context, typeof events>({
+    input: (_, event) => event.data.input
+})
+
+idle.on('updateInput').updateContext(updateInput)
 idle.on('updateInput').moveTo('debouncing')
 
 debouncing.after(500).moveTo('fetching')
-debouncing.on('updateInput').updateContext((context, event) => ({ ...context, input: event.data.input }))
+debouncing.on('updateInput').updateContext(updateInput)
 debouncing.on('updateInput').moveTo('debouncing')
 
-fetching.on('updateInput').moveTo('debouncing').updateContext((context, event) => ({ ...context, input: event.data.input }))
+fetching.on('updateInput').moveTo('debouncing').updateContext(updateInput)
 
 fetching.invokeAsyncCallback((context) => fetch('https://jsonplaceholder.typicode.com/todos/' + context.input)).onDone().moveTo('idle').fireAndForget((_, event) => console.log(event)).updateContext((context, event) => ({
     ...context,
