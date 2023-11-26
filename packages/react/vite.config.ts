ba719f05fb34b829682a0709b4b07db6109ad8b6
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-11-26 14:46:11 +0530

    feat: prepare react package for publishing

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import { resolve } from 'path'
+  import { defineConfig } from 'vite'
+  import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        watch: {
+
+        },
+        lib: {
+            entry: resolve(__dirname, 'lib/index.ts'),
+            name: 'index',
+            fileName: 'index',
+        },
+        rollupOptions: {
+            external: ['react'],
+            output: {
+                globals: {
+                    react: 'React'
+                }
+            }
+        }
+    },
+    // https://stackoverflow.com/questions/71982849/how-do-i-add-types-to-a-vite-library-build
+    plugins: [dts({ rollupTypes: true })]
 })
