3e0b546fb352fd7c0d2c32dacc52bcba0e3e8d92
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2023-12-05 14:24:39 +0530

    feat: add test suite

--- packages/core/tests/MachineConfig.test.ts
+++ packages/core/tests/MachineConfig.test.ts
@@ -1,5 +1,27 @@
-import {test, expect} from 'vitest';
+import {assert, describe, expect, test} from 'vitest';
+import {MachineConfig, createStates} from '../lib/index'
 
-test('1+1 should equal 2', () => {
-    expect(1+1).toBe(2)
+describe("Access to functions", () => {
+    const dummyMachine = new MachineConfig([], {}, [])
+    test('Should have getStates()', () => {
+        assert.isFunction(dummyMachine.getStates, 'getStates() should be a function')
+    })
+    test('Should have getConfig()', () => {
+        assert.isFunction(dummyMachine.getConfig, 'getConfig should be a function')
+    })
+})
+
+describe("States should be same as strings", () => {
+    const states = createStates('idle', 'loading', 'load end')
+    const dummyMachine = new MachineConfig(states, {}, [])
+    test('Should match the createStates params', () => {
+        expect(states).toEqual(Object.keys(dummyMachine.getStates()))
+    })
+});
+
+describe("Return of methods of MachineConfig instance", () => {
+    test('getStates() should return an empty objects when passed empty array', () => {
+        const dummyMachine = new MachineConfig([], {} ,[]);
+        expect({}).toEqual(dummyMachine.getStates())
+    })
 })
