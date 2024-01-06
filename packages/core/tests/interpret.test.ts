8c35b9a4049e266a293f984e8a1593046fbb7202
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2024-01-06 14:00:43 +0530

    update: interpret tests

--- packages/core/tests/interpret.test.ts
+++ packages/core/tests/interpret.test.ts
@@ -1,7 +1,34 @@
-import {describe, expect, test} from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { MachineConfig, createContext, createEvents } from '../lib'
+import { interpret } from '../lib/interpret/interpret'
+import { MACHINE_SUPER_STATE } from '../lib/constants'
 
-describe("test dummy case", () => {
-    test('Truth is truth', () => {
-        expect(true).toEqual(true)
+const context = createContext({
+    count: 0
+})
+const events = createEvents('INC', 'DEC')
+
+const statelessMachine = new MachineConfig([], context, events)
+statelessMachine.on('INC').updateContext({
+    count: context => context.count + 1
+})
+
+statelessMachine.on('DEC').updateContext({
+    count: context => context.count - 1
+})
+
+describe("interpret machine config", () => {
+    let state = {} as any;
+    const { start, subscribe, send } = interpret(statelessMachine)
+    subscribe(newState => { state = {...newState} })
+    const increment = () => send('INC');
+
+    test('cannot send events without starting machine', () => {
+        expect(increment).toThrowError('Please start machine before sending events')
+    })
+    test('receive an update on starting machine', () => {
+        start();
+        expect(state.value).toEqual(MACHINE_SUPER_STATE);
+        expect(state.context).toEqual({count: 0})
     })
 })
