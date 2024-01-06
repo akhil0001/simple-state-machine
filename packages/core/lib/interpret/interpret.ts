a7b5f1ef5282213dbc4fcffd6908a0b64ccb7bca
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2024-01-06 15:10:17 +0530

    update: add awake state to internal state

---
+++ packages/core/lib/interpret/interpret.ts
@@ -0,0 +1,45 @@
+import { TDefaultStates, TDefaultContext, IDefaultEvent } from "../types";
+import { EventEmitter } from "./EventEmitter";
+import { PubSub } from "./pubSub";
+import { MachineConfig } from "../MachineConfig";
+
+type TReturnState<U extends TDefaultStates, V extends TDefaultContext> = {
+    value: U[number],
+    context: V
+}
+
+type TSubscribeCallback<U extends TDefaultStates, V extends TDefaultContext> = (state: TReturnState<U, V>) => unknown
+
+type TInternalState = {
+    value: 'hibernating' | 'active'
+}
+
+export function interpret<U extends TDefaultStates, V extends TDefaultContext, W extends IDefaultEvent>(machineConfig: MachineConfig<U, V, W>) {
+    const { states, context } = machineConfig.getConfig();
+    const statePubSub = new PubSub<TReturnState<U, V>>();
+    const internalStatePubSub = new PubSub<TInternalState>({ value: 'hibernating' })
+    const eventEmitter = new EventEmitter()
+
+    function send(eventName: W[number]) {
+        if (internalStatePubSub.getStore().value === 'hibernating') {
+            throw new Error('Please start machine before sending events')
+        }
+        eventEmitter.emit(eventName)
+    }
+
+    function start() {
+        statePubSub.publish({ value: Object.keys(states)[0], context })
+        internalStatePubSub.publish({value: 'active'})
+    }
+
+    function subscribe(cb: TSubscribeCallback<U, V>) {
+        function unsubscribe() {
+            statePubSub.unsubscribe(cb);
+        }
+        statePubSub.subscribe(cb);
+        return unsubscribe
+    }
+
+    return { start, send, subscribe }
+}
+
