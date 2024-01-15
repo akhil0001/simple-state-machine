1701d4136e1fa7eb0b99220fd04aeaa5ea6bcc77
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2024-01-15 20:47:36 +0530

    update: package version to 0.1.0-alpha.3

--- packages/react/lib/index.ts
+++ packages/react/lib/index.ts
@@ -1,3 +1,3 @@
 export { useMachine } from './useMachine'
 export {useSharedMachine} from './useSharedMachine'
-export const SSM_REACT_VERSION = "0.1.0-alpha.2"
+export const SSM_REACT_VERSION = "0.1.0-alpha.3"
